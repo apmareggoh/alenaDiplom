b8c40ab53bc7923b60a12e43d40f308c51698bf0
Apmareggoh <Apmareggoh@bk.ru>
2016-05-12 23:14:35 +0300

    no message

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+$id = $_GET['id'];
+$dir = $_SERVER['DOCUMENT_ROOT'];
+require_once($dir . '/content/header.php');?>
+
+
+<div class="main-area" id="main_area">
+    <table class="table-content" cellpadding="0" cellspacing="0">
+        <tr>
+
+            <?php
+            require_once($dir . '/content/leftMenu.php');
+            ?>
+
+            <td class="center-column">
+
+
+                <div class="panel pagecontentall panel-pagecontent-90157 roundcorners">
+
+
+                    <h1 class="title" title="О нас">О нас</h1>
+                    <div class="panel-body">
+                        <div class="blog-text"><p style="FONT-SIZE: 16px"><strong>Компания Cosmetika</strong></p>
+                            <p>На протяжении долгого времени наша компания помогает женщинам выглядеть на все 100%. Сегодня наша продукция насчитывает более 150 наименований.</p>
+                            <p>Приобрести косметику вы можете только у наших представителей у себя в городе или став нашим представителем. Работа в нашей фирме - это сободный график, дополнительный заработок и независимоть от других.</p></div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+</div>
+<?php require_once($dir . '/content/footer.php');
+?>
+
