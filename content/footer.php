65015723da2988398e263a7cdee5c84517b0f273
apmareggoh <apmareggoh@bk.ru>
2016-05-12 23:16:32 +0300

    no message

--- content/footer.php
+++ content/footer.php
@@ -9,7 +9,7 @@
 
 <div id="footer">
 <div class="footer">
-    <center><a href="about.htm">О нас</a> &nbsp;&bull;&nbsp; <a href="contacts.htm">Контакты</a> &nbsp;&nbsp;&nbsp;&copy; 2012 &laquo;Cosmetika&raquo; - Все права защищены.</center>
+    <center><a href="/about.php">О нас</a> &nbsp;&bull;&nbsp; <a href="/contacts.php">Контакты</a> &nbsp;&nbsp;&nbsp;&copy; 2012 &laquo;Cosmetika&raquo; - Все права защищены.</center>
 
 
 </div>
